5c3f542e760cfe76aaee3dc9d9e1bbc3d8afa022
unknown <lucasdinizrodrigues123@gmail.com>
2024-11-25 22:27:03 -0300

    EndPoint para categorias de ocorrencias

--- pmc/src/main/java/com/br/protegemeucerrado/ocorrencia/service/OcorrenciaService.java
+++ pmc/src/main/java/com/br/protegemeucerrado/ocorrencia/service/OcorrenciaService.java
@@ -4,34 +4,45 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.br.protegemeucerrado.ocorrencia.DAO.CategoriaDAO;
 import com.br.protegemeucerrado.ocorrencia.DAO.OcorrenciaDAO;
+import com.br.protegemeucerrado.ocorrencia.model.Categoria;
 import com.br.protegemeucerrado.ocorrencia.model.Ocorrencia;
 
 @Service
 public class OcorrenciaService {
-    
+
     private OcorrenciaDAO ocDao;
-    public OcorrenciaService(OcorrenciaDAO ocDao){
+    private CategoriaDAO catDao;
+
+    public OcorrenciaService(OcorrenciaDAO ocDao, CategoriaDAO catDao) {
         this.ocDao = ocDao;
+        this.catDao = catDao;
     }
 
-    public Ocorrencia cadastrarOcorrencia(Ocorrencia oc){
+    public Ocorrencia cadastrarOcorrencia(Ocorrencia oc) {
         ocDao.save(oc);
         return oc;
     }
 
-    public Ocorrencia editarOcorrencia (Ocorrencia oc){
+    public Ocorrencia editarOcorrencia(Ocorrencia oc) {
         ocDao.save(oc);
         return oc;
     }
 
-    public Boolean excluirOcorrencia(Integer id){
+    public Boolean excluirOcorrencia(Integer id) {
         ocDao.deleteById(id);
         return true;
     }
 
-    public List<Ocorrencia> listarOcorrencias(){
+    public List<Ocorrencia> listarOcorrencias() {
         List<Ocorrencia> oc = ocDao.findAll();
         return oc;
     }
+
+    public List<Categoria> listarCategorias() {
+        List<Categoria> catOc = catDao.findAll();
+        return catOc;
+    }
+
 }
