9f12317d75784a538ca1fee56fa07cff264733f9
Xnijinv4X <vitorsiega9123@gmail.com>
2024-11-28 08:53:26 -0300

    feat: correção na estrutura

--- src/main/java/com/br/protegemeucerrado/ProtegeMeuCerrado.java
+++ src/main/java/com/br/protegemeucerrado/ProtegeMeuCerrado.java
@@ -2,9 +2,8 @@ package com.br.protegemeucerrado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication()
 public class ProtegeMeuCerrado {
 
     public static void main(String[] args) {
