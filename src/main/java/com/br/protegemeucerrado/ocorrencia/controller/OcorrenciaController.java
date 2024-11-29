ef2c2d01480a3f64810dfd881913d78f3a267854
Xnijinv4X <vitorsiega9123@gmail.com>
2024-11-28 22:00:36 -0300

    feat: correções

--- src/main/java/com/br/protegemeucerrado/ocorrencia/controller/OcorrenciaController.java
+++ src/main/java/com/br/protegemeucerrado/ocorrencia/controller/OcorrenciaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,14 +18,13 @@ import com.br.protegemeucerrado.ocorrencia.model.Ocorrencia;
 import com.br.protegemeucerrado.ocorrencia.service.OcorrenciaService;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/ocorrencias")
 public class OcorrenciaController {
 
     @Autowired
     private OcorrenciaService ocServ;
 
-    @GetMapping
+    @GetMapping("/listar")
     public ResponseEntity<List<Ocorrencia>> listarOcorrencias() {
         return ResponseEntity.status(200).body(ocServ.listarOcorrencias());
     }
@@ -36,18 +34,18 @@ public class OcorrenciaController {
         return ResponseEntity.status(200).body(ocServ.listarCategorias());
     }
 
-    @PostMapping
+    @PostMapping("/cadastro")
     public ResponseEntity<Ocorrencia> cadastrarOcorrencia(@RequestBody Ocorrencia oc) {
         return ResponseEntity.status(201).body(ocServ.cadastrarOcorrencia(oc));
     }
 
-    @PutMapping
-    public ResponseEntity<Ocorrencia> editarOcorrencia(@RequestBody Ocorrencia oc){
+    @PutMapping("/editar")
+    public ResponseEntity<Ocorrencia> editarOcorrencia(@RequestBody Ocorrencia oc) {
         return ResponseEntity.status(200).body(ocServ.editarOcorrencia(oc));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> excluirOcorrencia(@PathVariable Integer id){
+    public ResponseEntity<?> excluirOcorrencia(@PathVariable Integer id) {
         ocServ.excluirOcorrencia(id);
         return ResponseEntity.status(200).build();
     }
